f2fb2e8658cb7167a670c670ccca40c35b3dc996
lajawanti <myemail>
2019-05-30 17:27:09 -0400

    Common middleware configured

--- api/server.js
+++ api/server.js
@@ -5,8 +5,16 @@ const express = require('express');
 //server
 const server = express();
 
+//all commonMiddleware applied..
+const commonMiddleware = require('./Middlewares/commonMiddlewares.js')
+commonMiddleware(server);
+
 server.get('/', (req, res) => {
     res.json('API running....Stock-price.');
+})
+
+server.get('/users', (req, res) => {
+    res.json('Will give all registered user after db ');
 })
 
 module.exports = server;
