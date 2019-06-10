d2e4f80b790fa12c169b0f1fb1f634b601ad2690
lajawanti <myemail>
2019-06-10 18:07:10 -0400

    app/__init__.py changed tp integrate Dash in Flask

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,57 @@
-from flask import Flask
+import dash
 from config import Config
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask_login import LoginManager
+from flask import Flask
+from flask_login import LoginManager, login_required
+from flask.helpers import get_root_path
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    register_dashapps(app)
+    register_extensions(app)
+    register_blueprints(app)
+
+    return app
 
 
-app = Flask(__name__)
-app.config.from_object(Config)
+def register_dashapps(app):
+    from app.dashapp1.layout import layout
+    from app.dashapp1.callbacks import register_callbacks
 
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+    # Meta tags for viewport responsiveness
+    meta_viewport = {"name": "viewport", "content": "width=device-width, initial-scale=1, shrink-to-fit=no"}
 
-login = LoginManager(app)
-login.login_view = 'login'
+    dashapp1 = dash.Dash(__name__,
+                         server=app,
+                         url_base_pathname='/dashboard/',
+                         assets_folder=get_root_path(__name__) + '/dashboard/assets/',
+                         meta_tags=[meta_viewport])
+
+    dashapp1.title = 'Dashapp 1'
+    dashapp1.layout = layout
+    register_callbacks(dashapp1)
+    _protect_dashviews(dashapp1)
 
 
-from app import views, models
+def _protect_dashviews(dashapp):
+    for view_func in dashapp.server.view_functions:
+        if view_func.startswith(dashapp.url_base_pathname):
+            dashapp.server.view_functions[view_func] = login_required(dashapp.server.view_functions[view_func])
+
+
+def register_extensions(server):
+    from app.extensions import db
+    from app.extensions import login
+    from app.extensions import migrate
+
+    db.init_app(server)
+    login.init_app(server)
+    login.login_view = 'main.login'
+    migrate.init_app(server, db)
+
+
+def register_blueprints(server):
+    from app.views import app
+
+    server.register_blueprint(app)
