b50820d214cff651d96e538000e030f266493c93
lajawanti <myemail>
2019-06-13 13:40:11 -0400

    changing init.py for Proxyfix for heroku

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,14 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from werkzeug.contrib.fixers import ProxyFix
+
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+app.wsgi_app = ProxyFix(app.wsgi_app)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
